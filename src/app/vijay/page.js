789cd9a39fd6d397c1273b25f65b6548aea59369
vijay0221 <vijaymaroju2003@gmail.com>
2023-08-17 17:04:20 +0530

    initial commit

---
+++ src/app/vijay/page.js
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Vijay() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h2> vijay Page</h2>
+    </main>
+  )
+}
