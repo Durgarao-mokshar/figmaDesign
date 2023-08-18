66971b98318899151e1d4d021de6c998c6dfe4b5
vijay0221 <vijaymaroju2003@gmail.com>
2023-08-18 17:40:14 +0530

    created three sections

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,13 @@
 import Image from "next/image";
 import Hero from "@/components/Home/Hero";
-
+import SectionImage from "@/components/Home/SectionImage";
+import Section3 from "@/components/Home/Section3";
 export default function Home() {
-  return <Hero />;
+  return(
+    <>
+    <Hero/>
+    <SectionImage/>
+    <Section3/>
+    </>   
+  ) ;
 }
--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -1,5 +1,27 @@
-export default function Hero(){
-    return <div>
-        
-    </div>
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
+export default function Hero() {
+  return (
+    <section className="bg-[#f4f076] border-2-b border-black pb-60   p-4 pt-40">
+        <div className=" max-w-7xl  mx-auto">
+            <div className="grid lg:grid-cols-2 grid-cols-1  gap-12  ">
+                <h1 className="text-6xl font-bold">Fueling eCommerce Triumphs</h1>
+
+                <div className="  place-self-end ">
+                    <div className="uppercase text-xs font-bold mb-2">
+                        About us
+                    </div>
+
+                    <div className="text-xs">
+                        We're  passionate about powering direct-to-consumer brands to their peak performance. Our skill set incluedes stategic paid and search marketing, dynamic brand development, and polished Email and SMS management, all with a sigulr aim -resilient and ongoing growth.
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+    </section>
+  )
 }
