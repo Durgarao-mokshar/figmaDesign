feb83f65a899ab741a15027969cc89da4076f147
vijay0221 <vijaymaroju2003@gmail.com>
2023-08-24 14:06:07 +0530

    added

--- src/components/Home/Section8.jsx
+++ src/components/Home/Section8.jsx
@@ -1,7 +1,7 @@
 export default function Section8() {
 	return (
 		<section className="  flex items-start px-4 py-24 max-w-8xl mx-auto  justify-between flex-wrap gap-10">
-			<div className="flex flex-col  gap-6 justify-center pr-20">
+			<div className="flex flex-col  gap-6 justify-center pr-20 sticky top-24">
 				<div className="font-bold text-3xl">Designs That Stand Out</div>
 				<div className="">
 					<a href="#" className="text-white bg-gradient-to-r from-red-500 to-indigo-600 py-2 px-4 inline-block rounded-lg">
@@ -10,7 +10,7 @@ export default function Section8() {
 				</div>
 			</div>
 
-			<div className=" flex flex-col gap-6 lg:max-w-3xl">
+			<div className=" flex flex-col gap-6 lg:max-w-3xl overflow-y-auto">
 				<div className="">
 					<img src="https://uploads-ssl.webflow.com/648b823e0020cfd32ac58fb7/649e53b65f0708ca6cf0d59c_Tile-Web2.jpg" className=" rounded-xl" />
 				</div>
